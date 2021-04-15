0a89eb8a59810af73cf36cb582f669adc76d2a01
Anton O <aotab91@gmail.com>
2021-04-15 21:27:29 +0100

    Deleted unnecessary styles in Feed component

--- src/components/Feed/styles.js
+++ src/components/Feed/styles.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
   width: 65%;
@@ -8,35 +7,4 @@ export const Container = styled.div`
   justify-content: center;
   align-items: flex-start;
   flex-wrap: wrap;
-`;
-
-export const PhotoCard = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 5px;
-`;
-
-export const PhotoCardHeader = styled(Link)`
-  display: flex;
-  align-items: center;
-  text-decoration: none;
-  color: #000;
-  font-size: 18px;
-`;
-
-export const SmallProfilePicture = styled.img`
-  border: 2px solid #bf0000;
-  border-radius: 50%;
-  margin: 10px;
-`;
-
-export const PhotoCardFooter = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin: 10px 10px 50px 10px;
-`;
-
-export const Likes = styled.div`
-  display: flex;
-  align-items: center;
 `;
