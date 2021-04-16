023ccecf7b0276ba8579bd7645d749f0b747c806
Anton O <aotab91@gmail.com>
2021-04-16 21:11:06 +0100

    Fixed edge case for the tags of Collection component

--- src/components/Collection/index.js
+++ src/components/Collection/index.js
@@ -14,11 +14,13 @@ export const Collection = ({ data }) => {
       <CollectionInfo>
         <h3>{data.title}</h3>
         <span>{data.total_photos} photos</span>
-        <Labels>
-          <Label>{capitalizeFirstLetter(data.tags[0].title)}</Label>
-          <Label>{capitalizeFirstLetter(data.tags[1].title)}</Label>
-          <Label>{capitalizeFirstLetter(data.tags[2].title)}</Label>
-        </Labels>
+        {data.tags.length > 0 && (
+          <Labels>
+            <Label>{capitalizeFirstLetter(data.tags[0].title)}</Label>
+            <Label>{capitalizeFirstLetter(data.tags[1].title)}</Label>
+            <Label>{capitalizeFirstLetter(data.tags[2].title)}</Label>
+          </Labels>
+        )}
       </CollectionInfo>
     </Container>
   );
